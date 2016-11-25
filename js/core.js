24f30d4c9b5d84719cb572d0e35b786ce5756ca4
michaeljwilliams <me@mjwilliams.me>
2016-11-24 16:35:37 -0800

    set up crosswalk

--- js/core.js
+++ js/core.js
@@ -161,6 +161,15 @@ function readFromLocalStorage(cb) {
 // check that local storage is accessible
 checkLocalStorage();
 
+// Google Analytics
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-87818240-1', 'auto');
+  ga('send', 'pageview');
+
 // if no workouts in local storage, fetch defaults and add to local storage
 // else read workouts from local storage
 if(!localStorage.getItem("workouts")) {
